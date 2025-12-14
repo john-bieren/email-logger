cb4742be7b5f0095ad35acbcb737fe1cb3ffaa9d
John Bieren <jbieren64@gmail.com>
2025-12-13 21:54:01 -0800

    Lock dependencies again

--- update.ps1
+++ update.ps1
@@ -7,15 +7,7 @@ if (-not (Test-Path .venv)) { python -m venv .venv }
 python -m pip install --upgrade pip
 
 # install dependencies
-$Dependencies = @(
-    "openpyxl>=3.1.5,<4.0.0",
-    "pandas>=2.3.3,<3.0.0",
-    "pypdf>=6.4.0,<7.0.0",
-    "tqdm>=4.67.1,<5.0.0"
-)
-foreach ($item in $Dependencies) {
-    python -m pip install $item
-}
+python -m pip install -r requirements.txt
 
 # deactivate virtual environment
 deactivate
